f098b1fa844ef5912cbb864ecd3b27124b8d35f5
aqib-mulla <40908331+aqib-mulla@users.noreply.github.com>
2024-05-24 15:03:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,16 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage});
 
-const PORT = 3001;
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL,{
+useNewUrlParser: true,
+useUnifiedTopology: true,
+})
+.then(()=>{
+app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+})
+.catch((error)=> console.log(`${error} did not connect`))
 
 
 
