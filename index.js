7f524f377fd4fc25d9ee1c6add92ef974817e4a5
aqib-mulla <40908331+aqib-mulla@users.noreply.github.com>
2024-05-24 14:29:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 // Allow requests from the frontend origin (http://localhost:3000)
 app.use(cors({ origin: 'http://localhost:3000' }));
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+// app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use(express.static(path.join(__dirname, './build')));
 
 
 //FILE  STORAGE CONFIG
